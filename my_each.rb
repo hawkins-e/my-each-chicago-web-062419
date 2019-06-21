2e85f2e586014e360353ad3234b88ae511ef6b1b
Erica Hawkins <ehawk901@gmail.com>
2019-06-20 22:00:36 -0500

    My_Each exit! My_Each

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
-end
+def my_each(word)
+ 
+    i=0
+
+     while i < word.length
+        yield (word[i])
+         i = i+ 1
+        end 
+    word
+      end 
